3eab1990ac6c233fef879e8be305faacf1ae1c26
ghk01214 <ghk01214@naver.com>
2021-04-08 17:42:12 +0900

    4월 8일(목)

--- 2021-1 STL/2021-1 STL/main.cpp
+++ 2021-1 STL/2021-1 STL/main.cpp
@@ -2,60 +2,36 @@
 // 4월 8일 목요일 (6주 2일)
 // 
 // vector<int>에서 짝수만 삭제한다
-// vector<int> 중간에 원소를 추가해 본다
+// ㄴ remove가 지우는건 진짜가 아니다.
+// ㄴ 실제 지우는 건 자료구조가 해야 한다.
+// 
+// 중간고사 - 4월 22일 목요일 (8주 2일)
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+#include <deque>
 #include "pch.h"
 #include "save.h"
-#include <memory>
-#include <random>
+#include "String.h"
 
-// [문제] "Dog 만객체"에 바이너리 모드로 저장되어 있는 Dog 10000 객체를 읽어라
-// id 오름차순으로 정렬해서 출력하라
-
-std::uniform_int_distribution<> uid;
-std::default_random_engine dre;
-
-using namespace std::literals::string_literals;
-
-class Dog
-{
-private:
-	std::string name;
-	int id;
-	static int cnt;
-	friend std::ostream& operator<<(std::ostream& os, const Dog& dog);
-public:
-	Dog() : id{ uid(dre) }
-	{
-		name = "Dog"s;
-		name += std::to_string(cnt);
-
-		++cnt;
-	}
-	~Dog() {}
-};
-
-int Dog::cnt = { 1 };
-
-std::ostream& operator<<(std::ostream& os, const Dog& dog)
-{
-	os << "이름: " << dog.name << ", 아이디: " << dog.id;
-
-	return os;
-}
+// [문제] v에서 홀수를 제거하라
 
 int main()
 {
-	std::ifstream in{ "Dog 만객체" };
+	std::deque<String> d;
 
-	std::unique_ptr<Dog[]> dogs{ new Dog[10000] };
+	d.emplace(d.end(), "back");
+	d.emplace(d.end(), "front");
 
-	in.read((char*)dogs.get(), sizeof(Dog) * 10000);
-
-	for (int i = 0; i < 10000; ++i)
+	for (const String& s : d)
 	{
-		std::cout << dogs[i] << std::endl;
+		std::cout << s << std::endl;
+	}
+
+	d.emplace(d.end(), "middle");
+
+	for (const String& s : d)
+	{
+		std::cout << s << std::endl;
 	}
 
 	//Save("main.cpp");
