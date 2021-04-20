2ad14d06640feba19a785755acb0e09c06d7a971
ghk01214 <ghk01214@naver.com>
2021-04-20 15:17:27 +0900

    4월 20일(화)

--- 2021-1 STL/2021-1 STL/String.h
+++ 2021-1 STL/2021-1 STL/String.h
@@ -11,27 +11,54 @@
 
 //#define 관찰			// 이 주석을 풀면 special member의 동작을 알 수 있다.
 
+// 2021. 4. 20
+// String이 외부에 제공하는 반복자
+class String_iterator
+{
+public:
+	String_iterator(char* p) : p{ p } {}
+public:
+	//int operator+(const String_iterator& right) const { return right.p + p; }
+	int operator-(const String_iterator& right) const { return right.p - p; }
+public:
+	String_iterator& operator++() { ++p; return *this; }
+	String_iterator& operator--() { --p; return *this; }
+public:
+	bool operator==(const String_iterator& right) const { return right.p == p; }
+	bool operator!=(const String_iterator& right) const { return right.p != p; }
+public:
+	bool operator<(const String_iterator& right) { return p < right.p; }
+private:
+	char* p{ nullptr };
+};
+
 class String
 {
-private:
-	size_t num;							// 확보한 자원의 수
-	char* p;							// 확보한 자원의 위치
-
-	friend std::ostream& operator<<(std::ostream&, const String&);
+	using iterator = String_iterator;
+	using value_type = char;
 public:
 	String();
 	String(size_t n);
 	String(const char* str);
 	~String();
 public:
-	String(const String& other);					// 복사생성자
-	String& operator=(const String& other);			// 복사할당연산자
+	String(const String& other);							// 복사생성자
+	String& operator=(const String& other);					// 복사할당연산자
 	
-	String(String&& other) noexcept;							// 이동생성자
+	String(String&& other) noexcept;						// 이동생성자
 	String& operator=(String&& other)noexcept;				// 이동할당연산자
+public:
+	// 2021. 4. 20
+	iterator begin() { return iterator{ p }; }
+	iterator end() { return iterator{ p + num }; }
 public:
 	size_t size() const { return num; }
 
 	// 확보한 자원을 std::string처럼 이용할 수 있게 하자
 	std::string GetString() const;
+private:
+	size_t num;												// 확보한 자원의 수
+	char* p;												// 확보한 자원의 위치
+private:
+	friend std::ostream& operator<<(std::ostream&, const String&);
 };
--- 2021-1 STL/2021-1 STL/main.cpp
+++ 2021-1 STL/2021-1 STL/main.cpp
@@ -1,36 +1,37 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// 4월 15일 목요일 (7주 2일)
+// 4월 20일 화요일 (8주 1일)
 // 
-// 1. sequence container
-//	원소를 삭제/추가하는 동작이 O(1)
-//    - list
-//    - forward_list
+// 반복자 : 책 31 ~ 41, 123 ~ 136, Design Pattern 중 iterator pattern
+//  반복자는 포인터를 추상화한 것이다.
+//  반복자처럼 행동하는 것은 반복자이다.
+//  반복자는 container가 제공하는 인터페이스이다.
 // 
 // 중간고사 - 4월 22일 목요일 (8주 2일)
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-#include <list>
+#include <vector>
 #include <iterator>
+#include <list>
+#include <forward_list>
+#include <string>
 #include "pch.h"
 #include "save.h"
 #include "String.h"
 
-// [도전 문제] "main.cpp" 파일에 있는 단어를 뒤집어 출력하라.
-// ex) gnisu scapseman ;dts
+// [문제] String이 반복자를 리턴한다.
+// sort() 가 문제없이 실행되도록 반복자에게 필요한 함수들을 오버로딩하라
+
+template<typename iter>
+void showIteratorCategory(const iter&)
+{
+	std::cout << typeid(std::iterator_traits<iter>::iterator_category).name() << std::endl;
+}
 
 int main()
 {
-	std::list<int> l1{ 3, 7, 5, 1, 9, 7, 7, 7, 7, 7, 7 };
-	
-	// unique 함수는 연속된 인근의 원소들만 지운다.
-	l1.unique();
+	String s{ "1357924680" };
 
-	for (int a : l1)
-	{
-		std::cout << a << " ";
-	}
+	std::sort(s.begin(), s.end());
 
-	std::cout << std::endl;
-
-	Save("main.cpp");
+	//Save("main.cpp");
 }
