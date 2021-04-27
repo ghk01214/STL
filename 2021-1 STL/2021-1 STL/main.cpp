15508ec80112fbec9c76d1a3c8e763d8e9dadeac
ghk01214 <ghk01214@naver.com>
2021-04-27 15:18:53 +0900

    4월 27일(화)

--- 2021-1 STL/2021-1 STL/main.cpp
+++ 2021-1 STL/2021-1 STL/main.cpp
@@ -1,19 +1,16 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// 4월 20일 화요일 (8주 1일)
+// 4월 27일 화요일 (9주 1일)
+// 
+// container, algorithm, 그리고 이 둘을 연결하는 iterator
 // 
 // 반복자 : 책 31 ~ 41, 123 ~ 136, Design Pattern 중 iterator pattern
 //  반복자는 포인터를 추상화한 것이다.
 //  반복자처럼 행동하는 것은 반복자이다.
 //  반복자는 container가 제공하는 인터페이스이다.
-// 
-// 중간고사 - 4월 22일 목요일 (8주 2일)
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 #include <vector>
 #include <iterator>
-#include <list>
-#include <forward_list>
-#include <string>
 #include "pch.h"
 #include "save.h"
 #include "String.h"
@@ -31,7 +28,12 @@ int main()
 {
 	String s{ "1357924680" };
 
-	std::sort(s.begin(), s.end());
+	for (auto i{ s.rbegin() }; i != s.rend(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+
+	std::cout << std::endl;
 
 	//Save("main.cpp");
 }
