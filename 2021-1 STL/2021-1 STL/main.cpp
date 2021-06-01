2ea08bc7eafe9196d6640b62e915d1e89b436357
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2021-06-01 15:18:02 +0900

    6월 1일(목)

--- 2021-1 STL/2021-1 STL/main.cpp
+++ 2021-1 STL/2021-1 STL/main.cpp
@@ -1,5 +1,5 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-// 5월 27일 목요일 (13주 2일)
+// 5월 01일 화요일 (14주 1일)
 // 
 // 알고리즘 함수 살펴보기
 // 알고리즘 사용 예제(anagram)
@@ -9,24 +9,40 @@
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 #include <vector>
-#include <numeric>
+#include <map>
 #include "pch.h"		// iostream, algorithm
 #include "save.h"		// string_view, filesystem, fstream
 #include "String.h"		// random, string
 
-// [문제] "단어들.txt"를 벡터에 저장하라
-// 단어개 총 몇 개인지 출력하라
-// vector<string>에 담아라
+// [문제] 이 사전의 모든 애너그램 쌍을 출력하라
+
+struct ps : public std::pair<std::string, std::string>
+{
+	ps() = default;
+	ps(std::string s) : pair<std::string, std::string>(s, s) { std::sort(first.begin(), first.end()); }
+};
 
 int main()
 {
 	std::ifstream in{ "단어들.txt" };
-	std::vector<String> v{ std::istream_iterator<String>{in}, {} };
+	std::vector<ps> v{ std::istream_iterator<std::string>{in}, {} };
+	
+	std::sort(v.begin(), v.end(), [](const ps& a, const ps& b)
+		{
+			return a.first < b.first;
+		});
+
+	// 길이가 긴 순서대로 단어 10개를 출력하라.
+
+	std::cout << "Anagram을 찾는 프로그램이다." << std::endl;
+	std::cout << "총 " << v.size() << "개의 정렬된 단어가 있다." << std::endl << std::endl;
 
 	Save("main.cpp");
 
 	while (true)
 	{
+		auto pos{ std::adjacent_find(v.begin(), v.end()) };
 
+		
 	}
 }
